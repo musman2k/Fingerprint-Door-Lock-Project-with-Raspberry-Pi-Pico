d39e1bdc76e4004030b9ad74bc8d1ec19fd2de0d
Muhammad Usman Yousuf <89705011+musman2k@users.noreply.github.com>
2024-10-16 12:39:52 +0500

    Add files via upload

---
+++ code.py
@@ -0,0 +1,168 @@
+import time
+import board
+import busio
+from digitalio import DigitalInOut, Direction, Pull
+
+import adafruit_fingerprint
+
+led = DigitalInOut(board.GP15)
+led.direction = Direction.OUTPUT
+led.value = False
+
+pico_door_state = "Close"
+
+button = DigitalInOut(board.GP14)
+button.direction = Direction.INPUT
+button.pull = Pull.UP
+
+uart = busio.UART(board.GP4, board.GP5, baudrate=57600)
+
+finger = adafruit_fingerprint.Adafruit_Fingerprint(uart)
+##################################################
+
+# pylint: disable=too-many-branches
+def get_fingerprint_detail():
+    #Get a finger print image, template it, and see if it matches!
+    # This time, print out each error instead of just returning on failure
+    print("Getting image...", end="")
+    i = finger.get_image()
+    if i == adafruit_fingerprint.OK:
+        print("Image taken")
+    else:
+        if i == adafruit_fingerprint.NOFINGER:
+            print("No finger detected")
+        elif i == adafruit_fingerprint.IMAGEFAIL:
+            print("Imaging error")
+        else:
+            print("Other error")
+        return False
+
+    print("Templating...", end="")
+    i = finger.image_2_tz(1)
+    if i == adafruit_fingerprint.OK:
+        print("Templated")
+    else:
+        if i == adafruit_fingerprint.IMAGEMESS:
+            print("Image too messy")
+        elif i == adafruit_fingerprint.FEATUREFAIL:
+            print("Could not identify features")
+        elif i == adafruit_fingerprint.INVALIDIMAGE:
+            print("Image invalid")
+        else:
+            print("Other error")
+        return False
+
+    print("Searching...", end="")
+    i = finger.finger_fast_search()
+    # pylint: disable=no-else-return
+    # This block needs to be refactored when it can be tested.
+    if i == adafruit_fingerprint.OK:
+        print("Found fingerprint!")
+        return True
+    else:
+        if i == adafruit_fingerprint.NOTFOUND:
+            print("No match found")
+        else:
+            print("Other error")
+        return False
+
+
+# pylint: disable=too-many-statements
+def enroll_finger(location):
+    # Take a 2 finger images and template it, then store in 'location'
+    for fingerimg in range(1, 3):
+        if fingerimg == 1:
+            print("Place finger on sensor...", end="")
+        else:
+            print("Place same finger again...", end="")
+
+        while True:
+            i = finger.get_image()
+            if i == adafruit_fingerprint.OK:
+                print("Image taken")
+                break
+            if i == adafruit_fingerprint.NOFINGER:
+                print(".", end="")
+            elif i == adafruit_fingerprint.IMAGEFAIL:
+                print("Imaging error")
+                return False
+            else:
+                print("Other error")
+                return False
+
+        print("Templating...", end="")
+        i = finger.image_2_tz(fingerimg)
+        if i == adafruit_fingerprint.OK:
+            print("Templated")
+        else:
+            if i == adafruit_fingerprint.IMAGEMESS:
+                print("Image too messy")
+            elif i == adafruit_fingerprint.FEATUREFAIL:
+                print("Could not identify features")
+            elif i == adafruit_fingerprint.INVALIDIMAGE:
+                print("Image invalid")
+            else:
+                print("Other error")
+            return False
+
+        if fingerimg == 1:
+            print("Remove finger")
+            time.sleep(1)
+            while i != adafruit_fingerprint.NOFINGER:
+                i = finger.get_image()
+
+    print("Creating model...", end="")
+    i = finger.create_model()
+    if i == adafruit_fingerprint.OK:
+        print("Created")
+    else:
+        if i == adafruit_fingerprint.ENROLLMISMATCH:
+            print("Prints did not match")
+        else:
+            print("Other error")
+        return False
+
+    print("Storing model #%d..." % location, end="")
+    i = finger.store_model(location)
+    if i == adafruit_fingerprint.OK:
+        print("Stored")
+    else:
+        if i == adafruit_fingerprint.BADLOCATION:
+            print("Bad storage location")
+        elif i == adafruit_fingerprint.FLASHERR:
+            print("Flash storage error")
+        else:
+            print("Other error")
+        return False
+
+    return True
+
+
+##################################################
+
+
+def get_num():
+    # Use input() to get a valid number from 1 to 127. Retry till success!
+    i = 0
+    while (i > 127) or (i < 1):
+        try:
+            i = int(input("Enter ID # from 1-127: "))
+        except ValueError:
+            pass
+    return i
+
+
+while True:
+    res = get_fingerprint_detail()
+    if res:
+        if pico_door_state == "Close":
+            led.value = True
+            pico_door_state = "Open"
+        elif pico_door_state == "Open":
+            led.value = False
+            pico_door_state = "Close"
+    time.sleep(0.2)
+    
+    if not button.value:
+        time.sleep(0.1)
+        enroll_finger(get_num())
